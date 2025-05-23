432fbb718f5f4ba2f068380fd98be847e7039ef5
Alena Malinovskaya <malinovskaya.ale@gmail.com>
2025-05-23 23:14:29 +0200

    refact files

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ URL = "https://www.cd.cz/"
 incorrect_login ="testgmailcom"
 incorrect_password = "password"
 
-correct_login = "malinovskaya.ale@gmail.com"
-correct_password = "4925670m"
+correct_login = "malinovska.ale@gmail.com"
+correct_password = "12345678m"
